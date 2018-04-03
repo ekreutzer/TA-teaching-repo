20e2cfa59f6e12aec5872a5b99c3bb921be478df
Edward <ekreutze@uvm.edu>
2018-04-03 08:55:08 -0400

    divide

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 
 using namespace std;
 
+
+double divide(const int & a, const int & b){
+    return (a/b);
+}
 int main() {
     cout << "Hello, World!" << endl;
     cout << "My github repo" << endl;
+    cout << "8/4=" << divide(8,4) << endl;
     return 0;
 }
