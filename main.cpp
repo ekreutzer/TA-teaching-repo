b1ffa606facc1a673960d7e023f2022280620ea4
Edward <ekreutze@uvm.edu>
2018-04-03 10:32:20 -0400

    divide

--- main.cpp
+++ main.cpp
@@ -6,16 +6,17 @@ using namespace std;
 double divide(const int & a, const int & b){
     return (a/b);
 }
-int add(int a, int b) {
-    return a + b;
+int adds(int a, int b) {
+    return a - b;
 }
 
 int main() {
+    cout << "merge conflicts?" << endl;
     cout << "Hello, World!" << endl;
     cout << "My github repo" << endl;
 
 
-    cout << "3 + 5 = " << add(3, 5) << endl;
+    cout << "3 + 5 = " << adds(3, 5) << endl;
     cout << "8/4=" << divide(8,4) << endl;
     return 0;
 }
